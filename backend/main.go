6f694e25bac57375d80fb32becc9d095901a99dd
jareklupinski <jarek319@gmail.com>
2020-02-05 20:06:13 -0500

    simple demo

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var upgrader = websocket.Upgrader{}
+var greeted = make(map[string]int)
 
-func echo(w http.ResponseWriter, r *http.Request) {
+type Payload struct {
+	Name    string `json:"name"`
+	Message string `json:"message"`
+}
+
+func bot(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -25,17 +34,32 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
+		var payload Payload
+		err = json.Unmarshal(message, &payload)
+		var reply Payload
+		reply.Name = "Mount Sinai Help Assistant"
+		reply.Message = "Indeed it does!"
+		if greeted[payload.Name] == 0 {
+			if strings.Contains(payload.Message, "@") {
+				reply.Message = "Welcome " + payload.Message + "! How can we help you today?"
+				greeted[payload.Name]++
+			} else {
+				reply.Message = "Sorry, I did not understand that email address. Please make sure it is in the form of username@company.tld"
+			}
+		}
+		replyJSON, err := json.Marshal(reply)
+		err = c.WriteMessage(mt, replyJSON)
 		if err != nil {
 			log.Println("write:", err)
 			break
 		}
+
 	}
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/bot", bot)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'ws://localhost:8080/bot'
+
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state=({
+        counter: 0,
+        wsid: 12345
+      });
+  }
+
+  ws = new WebSocket(URL)
+
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected')
+    }
+    this.ws.onmessage = evt => {
+      // on receiving a message, add it to the list of messages
+      const message = JSON.parse(evt.data)
+      addResponseMessage(message.message);
+    }
+    this.ws.onclose = () => {
+      console.log('disconnected')
+      // automatically try to reconnect on connection loss
+      this.setState({
+        ws: new WebSocket(URL),
+      })
+    }
+    addResponseMessage("Welcome to the Mount Sinai Personal Care System! Please type in your email address so we can begin.");
+  }
+
+  handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    this.setState({ counter: this.state.counter + 1});
+    const message = { name: this.state.wsid, message: newMessage }
+    this.ws.send(JSON.stringify(message))
+    // Now send the message throught the backend API
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar={logo}
+          title="Mount Sinai Care System"
+          subtitle={this.state.counter}
+        />
+      </div>
+    );
+  } 
 }
 
 export default App;
